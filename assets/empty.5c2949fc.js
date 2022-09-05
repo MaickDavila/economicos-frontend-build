a04bd4abc576f2a8123f1f69a55d3f163a079f99
razthany <razthany15@gmail.com>
2022-09-05 10:51:20 -0500

    Cambio 05/09/2022

---
+++ assets/empty.5c2949fc.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./plugin-vue_export-helper.21dcd24c.js";import{r,o as t,c}from"./index.aac21346.js";const n={};function _(s,a){const e=r("router-view");return t(),c(e)}var f=o(n,[["render",_]]);export{f as default};
