2fb382f99d52b668b2d5372c87918c42152e2365
razthany <razthany15@gmail.com>
2022-09-06 10:41:48 -0500

    economicos usuario y rol2

--- assets/empty.203ba6c8.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./plugin-vue_export-helper.21dcd24c.js";import{r,o as t,c}from"./index.f1d1ed91.js";const n={};function _(s,a){const e=r("router-view");return t(),c(e)}var f=o(n,[["render",_]]);export{f as default};
