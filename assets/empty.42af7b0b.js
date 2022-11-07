08f68b4bde3b67313f12de3b6b7930514674511a
razthany <razthany15@gmail.com>
2022-11-07 13:48:34 -0500

    matriculas v2.1

--- assets/empty.42af7b0b.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./plugin-vue_export-helper.21dcd24c.js";import{r,o as t,c}from"./index.7f3b1c7b.js";const n={};function _(s,a){const e=r("router-view");return t(),c(e)}var f=o(n,[["render",_]]);export{f as default};
